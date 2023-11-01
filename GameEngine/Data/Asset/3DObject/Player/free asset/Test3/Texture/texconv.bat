511a5ffe3435796c41b61e6a666a0df0bf401888
vodinhva6 <dinhvan548@gmail.com>
2023-11-02 02:51:31 +0900

    tg

---
+++ GameEngine/Data/Asset/3DObject/Player/free asset/Test3/Texture/texconv.bat
@@ -0,0 +1,5 @@
+.\texconv.exe -f BC7_UNORM -m 0 -y -ft dds *.tga
+.\texconv.exe -f BC7_UNORM -m 0 -y -ft dds *.png
+
+REM .\texconv.exe -f BC7_UNORM -w 512 -h 512 -m 1 -y -ft dds *.tga
+REM .\texconv.exe -f BC3_UNORM -w 512 -h 512 -m 1 -y -ft dds *.png
